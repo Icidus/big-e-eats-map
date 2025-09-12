9b760d9411c28e82780d6533889ca0f26149c69c
Icidus <robert.e.oconnell@gmail.com>
2025-09-12 16:20:51 -0400

    Set base for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  base: '/big-e-eats-map/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
